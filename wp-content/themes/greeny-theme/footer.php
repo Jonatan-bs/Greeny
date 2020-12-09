cab3eeb86b0a033436b94bb9b1078bae8c87868f
Jonatan Shoshan <jonatan-bs@live.dk>
2020-12-09 15:21:32 +0100

    Added SOME buttons to footer

--- wp-content/themes/greeny-theme/footer.php
+++ wp-content/themes/greeny-theme/footer.php
@@ -1,4 +1,10 @@
 <footer>
+    <div class="some-buttons">
+        <a href="#"><img  class="symbol" src="<?php echo get_template_directory_uri() ?>/images/instagram.svg" alt="instagram" ></a>
+        <a href="#"><img  class="symbol" src="<?php echo get_template_directory_uri() ?>/images/twitter.svg" alt="twitter" ></a>
+        <a href="#"> <img  class="symbol" src="<?php echo get_template_directory_uri() ?>/images/facebook.svg" alt="facebook" ></a>
+
+    </div>
     <?php
         wp_nav_menu( 
             array(
