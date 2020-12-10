f5e202ec022304e50767a67977a882996aa18f5a
Jonatan Shoshan <jonatan-bs@live.dk>
2020-12-10 12:09:56 +0100

    Updated element spacing

--- wp-content/themes/greeny-theme/front-page.php
+++ wp-content/themes/greeny-theme/front-page.php
@@ -11,7 +11,7 @@
     </div>
 
 <!-- Starter pack CTA -->
-    <div class="starterpack pb">
+    <div class="starterpack mb">
         
         <div class="blue-circle"></div>
         <div class="blue-circle"></div>
@@ -27,67 +27,71 @@
         </p>
     </div>
 <!-- Products -->
-    <div class="content col-4 featured-products pb">
-        <?php
-            $meta_query  = WC()->query->get_meta_query();
-            $tax_query   = WC()->query->get_tax_query();
-            $tax_query[] = array(
-                'taxonomy' => 'product_visibility',
-                'field'    => 'name',
-                'terms'    => 'featured',
-                'operator' => 'IN',
-            );
-            
-            $args = array(
-                'post_type'           => 'product',
-                'post_status'         => 'publish',
-                'posts_per_page'      => 3,
-                'meta_query'          => $meta_query,
-                'tax_query'           => $tax_query,
-            );
-            
-            $featured_query = new WP_Query( $args );
+    <div class="featured-products mb center">
+        <div class="content col-4">
+            <?php
+                $meta_query  = WC()->query->get_meta_query();
+                $tax_query   = WC()->query->get_tax_query();
+                $tax_query[] = array(
+                    'taxonomy' => 'product_visibility',
+                    'field'    => 'name',
+                    'terms'    => 'featured',
+                    'operator' => 'IN',
+                );
                 
-            if ($featured_query->have_posts()) {
-            
-                while ($featured_query->have_posts()) : 
+                $args = array(
+                    'post_type'           => 'product',
+                    'post_status'         => 'publish',
+                    'posts_per_page'      => 3,
+                    'meta_query'          => $meta_query,
+                    'tax_query'           => $tax_query,
+                );
                 
-                    $featured_query->the_post();
+                $featured_query = new WP_Query( $args );
                     
-                    $product = get_product( $featured_query->post->ID );
-                    $price = $product->get_price_html();
-            
-                    ?>
-                    <div class="featured-product">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large', false );?>
-                            <img  class="image" src="<?echo $src[0];?>" alt="">
-                        </a>
-                        <a href="<?php the_permalink(); ?>">
-                            <p class="display-md color-dark  title"><?php the_title(); ?></p>
-                        </a>
-                        <p class="color-medium excerpt"><?php echo strip_tags( get_the_excerpt() ) ?></p>
-                        <div class="flex">
-                            <p class="price"><?php echo $price; ?></p>
-                            <img  class="add-to-cart-button pointer" data-id="<?php echo esc_attr( $product->get_id() ); ?>" src="<?php echo get_template_directory_uri() ?>/images/cart-add.svg" alt="add to cart" >
+                if ($featured_query->have_posts()) {
+                
+                    while ($featured_query->have_posts()) : 
+                    
+                        $featured_query->the_post();
+                        
+                        $product = get_product( $featured_query->post->ID );
+                        $price = $product->get_price_html();
+                
+                        ?>
+                        <div class="featured-product">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large', false );?>
+                                <img  class="image" src="<?echo $src[0];?>" alt="">
+                            </a>
+                            <a href="<?php the_permalink(); ?>">
+                                <p class="display-md color-dark  title"><?php the_title(); ?></p>
+                            </a>
+                            <p class="color-medium excerpt"><?php echo strip_tags( get_the_excerpt() ) ?></p>
+                            <div class="flex">
+                                <p class="price"><?php echo $price; ?></p>
+                                <img  class="add-to-cart-button pointer" data-id="<?php echo esc_attr( $product->get_id() ); ?>" src="<?php echo get_template_directory_uri() ?>/images/cart-add.svg" alt="add to cart" >
+                            </div>
                         </div>
-                    </div>
-                    <?php
+                        <?php
+                    
+                    
+                    endwhile;
                 
-                
-                endwhile;
-            
-            }
-        ?>
-    </div>
-    <div class="flexcenter">
-        <a class="page-link" href="<?php echo get_page_link( get_page_by_title( 'shop' )->ID ); ?>">Shop</a>
+                }
+            ?>
+        </div>
+    
+        <div class="flexcenter">
+            <a class="page-link" href="<?php echo get_page_link( get_page_by_title( 'shop' )->ID ); ?>">Shop</a>
+        </div>
     </div>
 <!-- Symbols -->
-    <div class="symbols-container pb">
-        <p class="display-xl color-secondary-light text-title" style="top: -210px; left: -260px;">All our juices are</p>
-        <img  class="arrow" src="<?php echo get_template_directory_uri() ?>/images/arrow.svg" alt="arrow" style="top: -40px; left: -160px;">
+    <div class="symbols-container mb">
+        
         <div class="symbols">
+            <p class="display-xl color-secondary-light text-title" style="top: 0px; left: 0px;">All our juices are</p>
+            <img  class="arrow" src="<?php echo get_template_directory_uri() ?>/images/arrow.svg" alt="arrow" style="top: 80px; left: 50px;">
             <div>
                 <img  class="arrow" src="<?php echo get_template_directory_uri() ?>/images/symbol1.png" alt="vegan" >
                 <p class="display-sm color-secondary-light ">Vegan</p>
