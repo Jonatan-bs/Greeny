6293c82c8e913855699d9d117ace5ba89dc01bd1
Jonatan Shoshan <jonatan-bs@live.dk>
2020-12-12 14:30:37 +0100

    Added repeatable fields for ingredients info - backend

--- wp-content/themes/greeny-theme/woocommerce/single-product.php
+++ wp-content/themes/greeny-theme/woocommerce/single-product.php
@@ -49,7 +49,43 @@ get_header('light'); ?>
 					
 				<!-- </div> -->
 				<div class="content text">
+				
 					<?php echo the_content() ?>
+					<!-- Ingredients -->
+					<div class="ingredients">
+						<?php 
+							$ingredients = get_post_meta( $post->ID, 'ingredients_fields', true );
+
+
+							foreach( $ingredients as $ingredient ){
+								$title = $ingredient['title'];
+								$text = $ingredient['text'];
+								$percentage = $ingredient['percentage'] . " %";
+								$image_attr =  wp_get_attachment_image_src(  esc_attr( $ingredient['image'], 'thumbnail' ));
+								?>
+									<div class="ingredient">
+										<div class="image-percentage">
+											<img 
+												class="symbol" 
+												src="<?php echo $image_attr[0] ?>" 
+												width="<?php echo $image_attr[1] ?>" 
+												height="<?php echo $image_attr[2] ?>" alt="" />
+
+											<p class="percentage"><?php echo $percentage ?></p>
+										</div>
+										<div class="title">
+											<?php echo $title ?>
+										</div>
+										<div class="text">
+											<?php echo $text ?>
+										</div>
+									</div>
+								<?php
+							}
+
+
+						?>
+					</div>
 				</div>
 
 			<?php endwhile; ?>
