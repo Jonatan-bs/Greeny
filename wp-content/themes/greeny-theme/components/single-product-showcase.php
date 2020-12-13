9ae73b9404fd487bcf6a6b386b2e97660bef371b
Jonatan Shoshan <jonatan-bs@live.dk>
2020-12-13 13:55:49 +0100

    moved single-product-showcase to component

---
+++ wp-content/themes/greeny-theme/components/single-product-showcase.php
@@ -0,0 +1,38 @@
+<?php 
+$product = $args['product'];
+?>
+
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'header-padding single-product-showcase', $product ); ?>>
+    
+    <div class="product-wrap">
+        <div class="product-image-width">
+            <div class="aspect-wrap">
+                <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large', false );?>
+                <img  class="product-image" src="<?php echo $src[0];?>" alt="">
+                <div class="circle"></div>
+            </div>
+        </div> 
+
+        <div class="info">
+            <p  class="title" > 
+                <?php echo the_title() ?>
+            </p>
+
+                    
+            <p class="price <?php if ($product->get_sale_price()) {?> onsale <?php } ?>">
+            <?php if ($product->get_sale_price()) {?>
+                <span class="sales-price"><?php echo $product->get_sale_price(), get_woocommerce_currency_symbol() ?></span>
+            <?php } ?>
+            <?php echo $product->get_regular_price(), get_woocommerce_currency_symbol()?>
+            </p>
+
+            <?php 
+                if ($product->is_purchasable() ) {
+            ?>   
+                <p  class="add-to-cart-button pointer" data-id="<?php echo esc_attr( $product->get_id() ); ?>" > 
+                    Add to cart
+                </p>
+            <?php } ?>
+        </div>
+    </div>
+</div>	
--- wp-content/themes/greeny-theme/woocommerce/single-product.php
+++ wp-content/themes/greeny-theme/woocommerce/single-product.php
@@ -8,46 +8,12 @@ get_header('light'); ?>
 <div id="single-product">
 	
 			<?php while ( have_posts() ) : ?>
-				<?php the_post(); ?>
-				<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'header-padding single-product-showcase', $product ); ?>>
-
-				<!-- <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>> -->
-					
-					<div class="product-wrap">
-						<div class="product-image-width">
-							<div class="aspect-wrap">
-								<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large', false );?>
-								<img  class="product-image" src="<?echo $src[0];?>" alt="">
-								<div class="circle"></div>
-							</div>
-						</div> 
-
-						<div class="info">
-							<p  class="title" > 
-								<?php echo the_title() ?>
-							</p>
-		
-									
-							<p class="price <?php if ($product->get_sale_price()) {?> onsale <?php } ?>">
-							<?php if ($product->get_sale_price()) {?>
-								<span class="sales-price"><?php echo $product->get_sale_price(), get_woocommerce_currency_symbol() ?></span>
-							<?php } ?>
-							<?php echo $product->get_regular_price(), get_woocommerce_currency_symbol()?>
-							</p>
-
-							<?php 
-								if ($product->is_purchasable() ) {
-							?>   
-								<p  class="add-to-cart-button pointer" data-id="<?php echo esc_attr( $product->get_id() ); ?>" > 
-									Add to cart
-								</p>
-							<?php } ?>
-						</div>
-					</div>
-				</div>	
-
-					
-				<!-- </div> -->
+				<?php the_post(); 
+				// $args = $product);
+?>
+				
+				<?php get_template_part('components/single-product-showcase', null, ['product' => $product]); ?>
+				
 				<div class="content text">
 					<section class="content mb">
 
