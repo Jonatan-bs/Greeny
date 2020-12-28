aa2f333f9fd55af6c85c9378fe9971d317051bf3
Jonatan Shoshan <jonatan-bs@live.dk>
2020-12-28 04:55:48 +0100

    Responsive front-page

--- wp-content/themes/greeny-theme/components/starterpack.php
+++ wp-content/themes/greeny-theme/components/starterpack.php
@@ -5,8 +5,8 @@
     <div class="blue-circle"></div>
 
     <div class="fruits">
-        <img src="<?php echo get_template_directory_uri() ?>/images/fruit/pineapple-blue.svg" alt="pineapple" style=" transform: translate(-70px, 10px) rotate(-15deg); width: 150px;">
-        <img src="<?php echo get_template_directory_uri() ?>/images/fruit/lime-blue.svg" alt="lime" style=" transform: translate(30px, 60px) rotate(25deg); width: 100px;">
+        <img src="<?php echo get_template_directory_uri() ?>/images/fruit/pineapple-blue.svg" alt="pineapple" style=" transform: translate(-70px, -10px) rotate(-15deg); width: 150px;">
+        <img src="<?php echo get_template_directory_uri() ?>/images/fruit/lime-blue.svg" alt="lime" style=" transform: translate(30px, 40px) rotate(25deg); width: 100px;">
     </div>
     <p class="display-md color-primary" >
         Start or complement your diet today <br>
