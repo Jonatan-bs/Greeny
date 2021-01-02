05d2a7750942462225cec2f69b23ff9aa0fd9329
Jonatan Shoshan <jonatan-bs@live.dk>
2021-01-02 18:47:41 +0100

    Made frontpage sachets single images

--- wp-content/themes/greeny-theme/components/frontpage-hero.php
+++ wp-content/themes/greeny-theme/components/frontpage-hero.php
@@ -3,5 +3,10 @@
         <p class="display-md color-contrast">a convenient way to add fruits and vegetables into your diet</p>
         <p class="body-sm color-light">Simply add water or any kind of milk and ENJOY!</p>            
     </div>
-    <img src="<?php echo get_template_directory_uri() ?>/images/hero-img.png" alt="sachets">
+    <div class="sachets">
+        <img class="sachet" src="<?php echo get_template_directory_uri() ?>/images/sachet-1.png" alt="sachets">
+        <img class="sachet" src="<?php echo get_template_directory_uri() ?>/images/sachet-2.png" alt="sachets">
+        <img class="sachet" src="<?php echo get_template_directory_uri() ?>/images/sachet-3.png" alt="sachets">
+    </div>
+    
 </div>
