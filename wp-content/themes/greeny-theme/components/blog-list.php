cdac3a9b0102597ced197a2a589cb2d23d21faf8
Jonatan Shoshan <jonatan-bs@live.dk>
2021-01-06 16:59:47 +0100

    Added pagination on blog page

--- wp-content/themes/greeny-theme/components/blog-list.php
+++ wp-content/themes/greeny-theme/components/blog-list.php
@@ -1,24 +1,25 @@
+
 <?php 
 
 $thisCategoryID = $wp_query->get_queried_object_id(); 
-
-
 $cat_id = get_query_var('cat')? get_query_var('cat') : -1 ;
+$currentPage = get_query_var('paged');
 
-$wpb_all_query = new WP_Query(
+$posts = new WP_Query(
     array(
         'post_type'=>'post', 
         'post_status'=>'publish',
         'cat'=>$cat_id, 
-        'posts_per_page'=>-1
+        'posts_per_page' => 10, // Max number of posts per page
+        'paged' => $currentPage 
         )
 ); ?>
 
-<?php if ( $wpb_all_query->have_posts() ) : ?>
+<?php if ( $posts->have_posts() ) : ?>
 
 
     <!-- the loop -->
-    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+    <?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
         <?php  get_template_part('components/partials/blog', 'single');?>
     <?php endwhile; ?>
     <!-- end of the loop -->
@@ -29,4 +30,16 @@ $wpb_all_query = new WP_Query(
 <?php else : ?>
     <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+
+<?php 
+    echo "<div class='page-nav-container'>" . paginate_links(array(
+        'total' => $posts->max_num_pages,
+        'prev_text' => __('<'),
+        'next_text' => __('>')
+    )) . "</div>";  
+?>
+
+
+
+
 
