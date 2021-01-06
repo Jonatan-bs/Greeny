303e9dbc22757d7cbb82ebd5787431d9fddeec5c
Jonatan Shoshan <jonatan-bs@live.dk>
2021-01-06 15:08:48 +0100

    New frontpage symbols

--- wp-content/themes/greeny-theme/components/symbols.php
+++ wp-content/themes/greeny-theme/components/symbols.php
@@ -4,22 +4,14 @@
         <p class="display-xl color-secondary-light text-title animate slideIn " style="top: 0px; left: 0px;">All our juices are</p>
         <img  class="arrow animate slideIn " src="<?php echo get_template_directory_uri() ?>/images/arrow.svg" alt="arrow" style="top: 80px; left: 50px;">
         <div class="animate slideIn sequence">
-            <img src="<?php echo get_template_directory_uri() ?>/images/symbol1.png" alt="vegan" >
-            <p class="display-sm color-secondary-light ">Vegan</p>
+            <img src="<?php echo get_template_directory_uri() ?>/images/symbol-vegan.svg" alt="vegan" >
         </div>
         <div class="animate slideIn sequence">
-            <img src="<?php echo get_template_directory_uri() ?>/images/symbol1.png" alt="vegan" >
-            <p class="display-sm color-secondary-light ">Vegan</p>
+            <img src="<?php echo get_template_directory_uri() ?>/images/symbol-glutenfree.svg" alt="glutenfree" >
         </div>
         <div class="animate slideIn sequence">
-            <img src="<?php echo get_template_directory_uri() ?>/images/symbol1.png" alt="vegan" >
-            <p class="display-sm color-secondary-light ">Vegan</p>
-        </div>
-        <div class="animate slideIn sequence">
-            <img src="<?php echo get_template_directory_uri() ?>/images/symbol1.png" alt="vegan" >
-            <p class="display-sm color-secondary-light ">Vegan</p>
-        </div>
-        
-        
+            <img src="<?php echo get_template_directory_uri() ?>/images/symbol-sugarfree.svg" alt="sugarfree" >
+        </div>        
+    
     </div>
 </div>
